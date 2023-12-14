d68af631e1b96fe9a7b83651b540f89dba58bf0b
dexit <dexit@dyc.lv>
2023-12-14 13:32:34 +0000

    yataa !

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+/*  build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        entryFileNames: `assets/[name][hash].[ext]`,
+        chunkFileNames: `assets/[name][hash].[ext]`,
+        assetFileNames: `assets/[name][hash].[ext]`
       }
     }
   }
+  */
 })
