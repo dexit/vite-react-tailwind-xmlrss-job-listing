8d70a36e8393f445322c150b33612274013db9d2
dexit <dexit@dyc.lv>
2023-12-14 13:13:39 +0000

    test3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOMO from 'react-dom/client'
 import MyComponent from './App'
 import './index.css'
 import './main.scss'
 
-ReactDOM.createRoot(document.getElementById('channel1')).render(
+ReactDOMO.createRoot(document.getElementById('channel1')).render(
   <React.StrictMode>
     <MyComponent />
   </React.StrictMode>
